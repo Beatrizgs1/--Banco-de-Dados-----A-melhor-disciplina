34e4fa3fcf6534226900c01c7c75700e6713564f
Beatrizgs1 <102838758+Beatrizgs1@users.noreply.github.com>
2023-09-12 09:51:06 -0300

    exercicio 4

--- listaExercicios01.sql
+++ listaExercicios01.sql
@@ -4,14 +4,23 @@
 SELECT titulo FROM livros;
 
 
+
 -- exercicio 2:
 SELECT nome FROM autores WHERE nascimento < '1900-01-01';
+
 
 
 -- exercicio 3:
 SELECT livros.titulo FROM livros
 JOIN autores ON livros.autor_id = autores.id
 WHERE autores.nome = 'J.K. Rowling';
+
+
+
+-- exercicio 4:
+SELECT alunos.nome FROM alunos
+JOIN matriculas ON alunos.id = matriculas.aluno_id
+WHERE matriculas.curso = 'Engenharia de Software';
 
 
 
