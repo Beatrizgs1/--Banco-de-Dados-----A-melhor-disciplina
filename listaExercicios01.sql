0c5ba2c0541ee709ed39eb7e2e8a9cd30961f71d
Beatrizgs1 <102838758+Beatrizgs1@users.noreply.github.com>
2023-09-12 09:57:18 -0300

    exercicio 6

--- listaExercicios01.sql
+++ listaExercicios01.sql
@@ -5,8 +5,10 @@ SELECT titulo FROM livros;
 
 
 
+
 -- exercicio 2:
 SELECT nome FROM autores WHERE nascimento < '1900-01-01';
+
 
 
 
@@ -20,6 +22,9 @@ WHERE autores.nome = 'J.K. Rowling';
 -- exercicio 4:
 SELECT alunos.nome FROM alunos
 JOIN matriculas ON alunos.id = matriculas.aluno_id
+WHERE matriculas.curso = 'Engenharia de Software';
+
+
 
 
 -- exercicio 5:
@@ -27,7 +32,15 @@ SELECT produto, SUM(receita) AS receita_total
 FROM vendas
 GROUP BY produto;
 
-WHERE matriculas.curso = 'Engenharia de Software';
+
+
+--exercicio 6:
+SELECT autores.nome, COUNT(livros.id) AS numero_de_livros
+FROM autores
+LEFT JOIN livros ON autores.id = livros.autor_id
+GROUP BY autores.nome;
+
+
 
 
 
